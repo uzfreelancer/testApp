2ce0a0bce75e6ef8619ff3c9f328c3a47b30d309
AndreWerdan <stupchuk.a@gmail.com>
2015-03-20 18:48:40 +0200

    commit by A.W

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
 var loger = require('morgan');
-/*
+
 function myCustomStack(req,res,next){
     //функция прогоняет запросы через себя.для примера считываем заголовки, айпи
     console.log(req.header('user-agent'));
+    console.log('hello');
     console.log(req.ip);
     next();
 }
-*/
+
 //app.use(myCustomStack);//for all
 app.use(loger('dev'));
 
